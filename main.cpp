1493596bb0474479331416bf3c4a0fcd94e660c9
Kirill <Kirill@DESKTOP-8GNOR6F>
2022-05-03 18:55:01 +0300

    updetint

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
-	cout << "hi world";
+	string line;
+	int counter = 0;
+	char letter;
+
+	cout << "Enter text: ";
+	getline(cin, line);
+	cout << "Enter letter: ";
+	cin >> letter;
+
+	for (unsigned int i = 0; i < line.length(); i++)
+		if (line[i] == letter)
+		{
+			counter++;
+		}
+	cout << "In the text: " << counter << " <" << letter << "> " << endl;
 	return 0;
 }
